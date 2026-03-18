3d3307beca725b143c89b32b34951e65dc4a0617
arinafomintseva45-lab <arinafomintseva45@gmail.com>
2026-03-18 19:56:24 +0100

    poprawa

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value){
     const result = [];
-    for ( let i = 0 ; i < numbers.lenght; i++){
+    for ( let i = 0 ; i < numbers.length; i++){
         if (numbers[i] > value){
             result.push(numbers[i]);
         }
